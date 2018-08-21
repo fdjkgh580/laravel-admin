dd1cefc7f3ed2d4c5cd12132daff7fe081a05b99
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-08-21 17:04:54 +0800

    後台登入介面建立初始

---
+++ resources/views/admin/login/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <base href="{{ $base }}">
+</head>
+<body>
+    <form method="post" action="admin/login">
+        @csrf
+        <input type="email" name="email" placeholder="email" autofocus>
+        <input type="password" name="password" placeholder="password">
+        <button>登入</button>
+    </form>
+</body>
+</html>
