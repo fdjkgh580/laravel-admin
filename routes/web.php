0b09d090faca67e2d0c2be0b9e2703d27715f07c
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-08-21 17:14:22 +0800

    建立前台登入

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('admin/login', 'Admin\LoginController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
