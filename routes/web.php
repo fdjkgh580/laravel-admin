8bb652fa804f7b94c37813357daab20a7e4ee291
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-08-22 10:02:36 +0800

    路由寫法修改

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Auth::routes();
 
+Route::get('/', function ()
+{
+    return view('welcome');
+});
 
-Route::resource('/admin/login', 'Admin\LoginController');
-Route::get('/admin/logout', 'Admin\LoginController@logout');
 
+//
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'Admin\DashboardController@index');
+
+// 後台管理
+Route::prefix('admin')->group(function ()
+{
+    Route::get('/', 'Admin\DashboardController@index');
+    Route::resource('login', 'Admin\LoginController');
+    Route::get('logout', 'Admin\LoginController@logout');
+});
